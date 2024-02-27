7acb3f1eb6e72d805d832bec95b5938212b4773e
Kobzarev Michael <mick_mick_mick@icloud.com>
2024-02-27 22:50:21 +0400

    regular

--- src/main/java/org/technosoft/test/Main.java
+++ src/main/java/org/technosoft/test/Main.java
@@ -1,9 +1,13 @@
 package org.technosoft.test;
 
+import java.time.LocalDateTime;
+
 public class Main {
 
     public static void main(String[] args) {
 
         System.out.println((long) (Math.random() * Integer.MAX_VALUE + 1));
+
+        System.out.println(String.join("-",LocalDateTime.now().toString(), String.valueOf(100_000L)));
     }
 }
