0ff9a037c6109a4a892b43dca53984b2c67b6190
Kobzarev Michael <mick_mick_mick@icloud.com>
2024-03-18 19:50:22 +0400

    regular

--- src/main/java/org/technosoft/test/Main.java
+++ src/main/java/org/technosoft/test/Main.java
@@ -6,8 +6,13 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println((long) (Math.random() * Integer.MAX_VALUE + 1));
+        System.out.println("Random Integer: " + (int) (Math.random() * Integer.MAX_VALUE + 1));
 
-        System.out.println(String.join("-",LocalDateTime.now().toString(), String.valueOf(100_000L)));
+        System.out.println("Random Long: " + (long) (Math.random() * Long.MAX_VALUE + 1));
+
+        String maxSize = String.join("-", LocalDateTime.now().toString(), String.valueOf(Long.MAX_VALUE));
+        System.out.printf("%s\nits length: %s%n", maxSize, maxSize.length());
+
+        System.out.printf("Id = %d-%s", 1_000L, LocalDateTime.now());
     }
 }
